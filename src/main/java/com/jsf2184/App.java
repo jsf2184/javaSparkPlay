b37f5679c8b3b4234bdb651e5167a2285026464e
jsf2184 <jsf2184@gmail.com>
2018-01-16 20:36:38 -0600

    Introduce log4j logging

--- src/main/java/com/jsf2184/App.java
+++ src/main/java/com/jsf2184/App.java
@@ -1,13 +1,17 @@
 package com.jsf2184;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger _log = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        _log.info("Hello World!");
     }
 }

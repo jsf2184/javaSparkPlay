af5467ba6adf5c25f5643af187322cec52e6946a
jsf2184 <jsf2184@gmail.com>
2018-01-16 20:22:23 -0600

    Initial Commit of intellij project

---
+++ src/main/java/com/jsf2184/App.java
@@ -0,0 +1,13 @@
+package com.jsf2184;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
